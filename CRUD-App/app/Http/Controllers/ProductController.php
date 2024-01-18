a40b98d75d517dd8f7dbe0ad1c2d5944433bb423
Yasmin <yasminbanu1605@gmail.com>
2024-01-18 16:54:19 +0530

    TASK : Initial commit

---
+++ CRUD-App/app/Http/Controllers/ProductController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Controllers;
+use Illuminate\Support\Facades\Validator;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+   
+    
+  
+}
